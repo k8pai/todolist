2b0027d860e600dea70b28e6e757dfd69041a531
k8pai <thek8pai@gmail.com>
2023-02-06 02:15:13 +0530

    updated about page

--- pages/about.js
+++ pages/about.js
@@ -1,44 +1,105 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
+import { IconContext } from 'react-icons'
+import { SiNextdotjs, SiReact, SiTailwindcss, SiGithub, SiTwitter, SiInstagram, SiLinkedin, SiDiscord, SiGmail } from 'react-icons/si'
+import { SlLocationPin } from 'react-icons/sl'
+import { TbBrandNextjs } from 'react-icons/tb'
 
 export default function about() {
-  return (
-    <div className='flex-grow flex items-center transition-all duration-150 ease-in'>
-        <div className='max-w-7xl w-full mx-auto font-semibold flex flex-wrap justify-center'>
-			<div className='p-5 m-2 sm:mx-2 sm:my-1 xsm:mx-2 xsm:my-1 max-w-3xl flex items-center rounded-md shadow-lg transition-all duration-200 ease-out bg-pritxt dark:bg-secbg h-fit'>
-				<div className='space-y-3'>
-					<h1 className='tracking-widest text-center text-2xl md:text-xl sm:text-lg xsm:text-lg text-pribg dark:text-pritxt'>About Dev</h1>
+	return (
+		<div className='flex-grow max-w-3xl w-full h-full flex flex-col justify-center mx-auto transition-all duration-150 ease-in'>
+			<div className='p-3 space-y-6 flex flex-col items-start'>
+				<div className='username space-y-2 mx-4'>
+					<h2 className='text-xl text-pribg dark:text-cyan-200 font-semibold'>sudarsan pai</h2>
+					<div className='userinfo flex items-center space-x-6'>
+						<div className=''>
+							<Link className='text-pribg dark:text-cyan-100' href={'https://twitter.com/k8pai'}>&#64;k8pai</Link>
+						</div>
+						<div className='flex items-center space-x-2'>
+							<SlLocationPin />
+							<h4 className='text-pribg dark:text-cyan-100'>Bangalore, India</h4>
+						</div>				
+					</div>
+				</div>
+					
+				<div className='flex flex-wrap items-center mx-4'>
+					<Link className='transition duration-200 p-2 m-1 rounded-md ring-2 ring-transparent hover:ring-pribg hover:dark:ring-pritxt' href={'https://github.com/k8pai'}>
+						<IconContext.Provider value={{ size: "1.2em", className: "global-class-name" }}>
+							<SiGithub />
+						</IconContext.Provider>
+					</Link>
+					<Link className='transition duration-200 p-2 m-1 rounded-md ring-2 ring-transparent hover:ring-pribg hover:dark:ring-pritxt' href={'https://twitter.com/k8pai'}>
+						<IconContext.Provider value={{ size: "1.2em", className: "global-class-name" }}>
+							<SiTwitter />
+						</IconContext.Provider>
+					</Link>
+					<Link className='transition duration-200 p-2 m-1 rounded-md ring-2 ring-transparent hover:ring-pribg hover:dark:ring-pritxt' href={'https://github.com/k8pai'}>
+						<IconContext.Provider value={{ size: "1.2em", className: "global-class-name" }}>
+							<SiInstagram />
+						</IconContext.Provider>
+					</Link>
+					<Link className='transition duration-200 p-2 m-1 rounded-md ring-2 ring-transparent hover:ring-pribg hover:dark:ring-pritxt' href={'https://linkedin.com/in/k8pai'}>
+						<IconContext.Provider value={{ size: "1.2em", className: "global-class-name" }}>
+							<SiLinkedin />
+						</IconContext.Provider>
+					</Link>
+					<Link className='transition duration-200 p-2 m-1 rounded-md ring-2 ring-transparent hover:ring-pribg hover:dark:ring-pritxt' href={'mailto:thek8pai@gmail.com'}>
+						<IconContext.Provider value={{ size: "1.2em", className: "global-class-name" }}>
+							<SiGmail />
+						</IconContext.Provider>
+					</Link>
+					<Link className='transition duration-200 p-2 m-1 rounded-md ring-2 ring-transparent hover:ring-pribg hover:dark:ring-pritxt' href={'https://discord.com/users/898949804024012850'}>
+						<IconContext.Provider value={{ size: "1.2em", className: "global-class-name" }}>
+							<SiDiscord />
+						</IconContext.Provider>
+					</Link>
+				</div>
+					
+				<div className='aboutme space-y-3 mx-4'>
+					<h1 className='text-xl text-pribg dark:text-cyan-200 font-semibold'>About Me.</h1>
 					<p className='text-pribg dark:text-sectxt tracking-wide word-0 text-lg sm:text-base xsm:text-base'>I&apos;m sudarsh, a Front-end developer based on bangalore, and obviously i love to code. Nothing much to add for now, just learning stuff, getting started with Nextjs and some js libraries.</p>
 				</div>
-				<div className='min-w-[200px] sm:hidden xsm:hidden '>
-					<Image
-						className='rounded-full p-3'
-						src={'/pic.png'}
-						width="200"
-						height="200"
-						alt='dev picture' />
+					
+				<div className='aboutme space-y-3 mx-4'>
+					<h1 className='text-xl text-pribg dark:text-cyan-200 font-semibold'>About Project.</h1>
+					<p className='text-pribg dark:text-sectxt tracking-wide word-0 text-lg sm:text-base xsm:text-base'>A simple todo-list web app, that can be used to manage tasks and todos. This project was made to be part of my portfolio building.</p>
+				</div>
+				
+				<div className='aboutme space-y-3 mx-4'>
+					<h1 className='text-xl text-pribg dark:text-cyan-200 font-semibold'>Tools Used.</h1>
+					<ul className='space-y-3 text-terbg dark:text-sectxt text-lg sm:text-base xsm:text-base'>
+						<div className='flex items-center space-x-2'>
+							<IconContext.Provider value={{ size: "1.2em" }}>
+								<SiNextdotjs />
+							</IconContext.Provider>
+							<Link className='transition duration-200 text-blue-400 hover:cursor-pointer hover:text-blue-600' href={"https://nextjs.org/"}> Nextjs</Link>
+						</div>
+						
+						<div className='flex items-center space-x-2'>
+							<IconContext.Provider value={{ size: "1.2em" }}>
+								<TbBrandNextjs />
+							</IconContext.Provider>
+							<Link className='transition duration-200 text-blue-400 hover:cursor-pointer hover:text-blue-600' href={"https://github.com/pacocoursey/next-themes"}> Next-themes</Link>
+						</div>
+						
+						<div className='flex items-center space-x-2'>
+							<IconContext.Provider value={{ size: "1.2em" }}>
+								<SiTailwindcss />
+							</IconContext.Provider>
+							<Link className='transition duration-200 text-blue-400 hover:cursor-pointer hover:text-blue-600' href={"https://tailwindcss.com/"}> tailwindcss</Link>
+						</div>
+
+						<div className='flex items-center space-x-2'>
+							<IconContext.Provider value={{ size: "1.2em" }}>
+								<SiReact />
+							</IconContext.Provider>
+							<Link className='transition duration-200 text-blue-400 hover:cursor-pointer hover:text-blue-600' href={"https://react-icons.github.io/react-icons/"}> React-icons</Link>
+						</div>
+						
+					</ul>
 				</div>
 			</div>
-			<div className='p-5 m-2 sm:mx-2 sm:my-1 xsm:mx-2 xsm:my-1 my-1 w-fit rounded-md tracking-wide space-y-3 shadow-lg transition-all duration-200 ease-out bg-pritxt dark:bg-secbg h-fit'>
-				<h1 className=' text-center text-2xl md:text-xl sm:text-lg xsm:text-lg text-pribg dark:text-pritxt'>About Project</h1>
-				<div className='text-pribg dark:text-sectxt text-lg sm:text-base xsm:text-base'>
-					<p className=''>A simple todo-list web app, that can be used to manage tasks and todos.</p>
-					<p className=''>This project was made to be part of my portfolio building.</p>
-				</div>
-			</div>
-			<div className='p-5 m-2 sm:mx-2 sm:my-1 xsm:mx-2 xsm:my-1 w-fit space-y-3 rounded-md tracking-wide shadow-lg transition-all duration-200 ease-out bg-pritxt dark:bg-secbg h-fit'>
-				<h1 className='text-center text-pribg dark:text-pritxt text-2xl md:text-xl sm:text-lg xsm:text-lg'>Tools used</h1>
-				<ul className='text-terbg dark:text-sectxt text-lg sm:text-base xsm:text-base'>
-					<h1 className=''><Link className='transition duration-200 text-blue-400 hover:cursor-pointer hover:text-blue-600' href={"https://nextjs.org/"}> Nextjs</Link> - Framework.</h1>
-					{/* <h1 className=''><Link className='transition duration-200 text-blue-400 hover:cursor-pointer hover:text-blue-600' href={"https://next-auth.js.org/"}> Next-auth</Link> - Authorization.</h1> */}
-					<h1 className=''><Link className='transition duration-200 text-blue-400 hover:cursor-pointer hover:text-blue-600' href={"https://github.com/pacocoursey/next-themes"}> Next-themes</Link> - For Themes.</h1>
-					<h1 className=''><Link className='transition duration-200 text-blue-400 hover:cursor-pointer hover:text-blue-600' href={"https://tailwindcss.com/"}> tailwindcss</Link> - Customizing UI.</h1>
-					<h1 className=''><Link className='transition duration-200 text-blue-400 hover:cursor-pointer hover:text-blue-600' href={"https://react-icons.github.io/react-icons/"}> React-icons</Link> - To Generalize theme icons.</h1>
-					<h1 className=''><Link className='transition duration-200 text-blue-400 hover:cursor-pointer hover:text-blue-600' href={"https://jaketrent.github.io/react-social-icons/"}> React-social-icons</Link> - For social link icons.</h1>
-				</ul>
-			</div>
-        </div>
-    </div>
-  )
+		</div>
+	)
 }
