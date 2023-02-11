02782460a8c746d33c9141e0d6f5eead4191f803
k8pai <thek8pai@gmail.com>
2023-02-12 00:10:59 +0530

    ui updated

--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,33 @@
-import { useEffect, useState } from 'react'
-import { listIdGenerator } from "../lib/generator"
-import ListInput from '../components/ListInput'
-import TodoCard from '../components/TodoCard'
+import { useEffect, useState } from 'react';
+import { listIdGenerator } from '../lib/generator';
+import ListInput from '../components/ListInput';
+import TodoCard from '../components/TodoCard';
 
 export default function Home() {
-
 	const [lists, setLists] = useState(() => {
 		if (typeof window !== 'undefined') {
-			return JSON.parse(localStorage.getItem('List')) || [{id: '000-00-0000', name: "default"}];
+			return (
+				JSON.parse(localStorage.getItem('List')) || [
+					{ id: '000-00-0000', name: 'default' },
+				]
+			);
 		}
-		return [{id: '000-00-0000', name: "default"}]
-	})
+		return [{ id: '000-00-0000', name: 'default' }];
+	});
 
 	useEffect(() => {
 		localStorage.setItem('List', JSON.stringify(lists));
-	}, [lists])
+	}, [lists]);
 
 	const addList = (newName) => {
-		const data = { id: listIdGenerator(), name: newName }
-		setLists([...lists, data])
-	}
+		const data = { id: listIdGenerator(), name: newName };
+		setLists([...lists, data]);
+	};
 
 	const renameList = (id, newName) => {
-		const updatedList = lists.map(item => {
+		const updatedList = lists.map((item) => {
 			if (item.id === id) {
-				return {...item, name: newName};
+				return { ...item, name: newName };
 			}
 			return item;
 		});
@@ -34,23 +37,28 @@ export default function Home() {
 
 	const deleteList = (id) => {
 		localStorage.removeItem(`tasks[${id}]`);
-		setLists(val => val.filter(el => el.id !== id));
-	}
+		setLists((val) => val.filter((el) => el.id !== id));
+	};
 
-	const todoHeaderProps = { addList }
-	const InputTodoNameProps = { renameList, deleteList }
-    return (
-        <>
-            <div className='flex-grow flex justify-center transition ease-linear duration-150 bg-pritxt dark:bg-pribg'>
-                <div className='mx-auto w-full h-fit'>
-                    <ListInput {...todoHeaderProps}/>
-					<div className='mt-[50px] flex flex-wrap'>
-						{lists.map(item => (
-							<TodoCard key={item.id} item={item} {...InputTodoNameProps} />
-						))}
+	const todoHeaderProps = { addList };
+	const InputTodoNameProps = { renameList, deleteList };
+	return (
+		<>
+			<div className="flex-grow flex justify-center transition ease-linear duration-150 bg-pritxt dark:bg-pribg">
+				<div className="mx-auto w-full h-fit">
+					<ListInput {...todoHeaderProps} />
+					<div className="mt-[50px] flex flex-wrap xsm:justify-center sm:justify-center md:justify-center lg:justify-center">
+						{lists &&
+							lists?.map((item) => (
+								<TodoCard
+									key={item.id}
+									item={item}
+									{...InputTodoNameProps}
+								/>
+							))}
 					</div>
-                </div>
-            </div>
-        </>
-    )
+				</div>
+			</div>
+		</>
+	);
 }
